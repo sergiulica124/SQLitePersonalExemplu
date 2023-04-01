c4947a4652625e2f10073356ef57258fc0321ba8
sergiulica124 <85820061+sergiulica124@users.noreply.github.com>
2023-04-01 21:19:50 +0300

    Update NoteAdapter.java
    schimb

--- app/src/main/java/codewithcal/au/sqliteandroidstudiotutorial/NoteAdapter.java
+++ app/src/main/java/codewithcal/au/sqliteandroidstudiotutorial/NoteAdapter.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class NoteAdapter extends ArrayAdapter<Note>
 {
+private String tugo;
     public NoteAdapter(Context context, List<Note> notes)
     {
         super(context, 0, notes);
